92fe73a3d5ba31a278b6bde374927f4c8066d523
Your Name <you@example.com>
2021-12-01 11:32:59 +0100

    commit all

--- V01/DA/MySqlComponents/Dao.php
+++ V01/DA/MySqlComponents/Dao.php
@@ -20,15 +20,14 @@ class Dao
     // basic params strings
     public $FE; // Fundamental Entity
     public $FEFs; // Fundamental Entity Fields
+    public $VM; // Values Matrix 
+    public $OFs; // Ordering  Fields
     // derived params strings
     public $DEs; // Descriptive Entities
-    public $EntitySql; // Entity
-    public $FilterSql; // Filter
     public $EFs; //  Entity Fields
-    public $OFs; // Ordering  Fields
-    public $VM; // Values Matrix 
-    public $LU; // List for Update 
-    public $DI; // Delete Ids 
+    public $EntitySql; // Entity
+    public $FilterSql; // Filter Sql 
+    public $LUSql; // List for Update Sql
 
     public $IdFE;
     public $Nam;
@@ -42,14 +41,15 @@ class Dao
     }
 
     // read all testEntitys
-    public function selectAll()
+    public function select()
     {
         try {
 
             $query = "
-            SELECT ".$EFs."
-            FROM ".$EntitySql."
-            ORDER BY ".$OFs."
+            SELECT ".$this->EFs."
+            FROM ".getEntitySql($FE, $FEFs)."
+            WHERE ".getIdFilterSql($FE, $FilterIds,$FilterType)."
+            ORDER BY ".$this->OFs."
             ";
             //throw new exception($query."\n");
             // prepare query statement
@@ -61,33 +61,6 @@ class Dao
             return false;
         }
     }
-
-    // get single testEntity data
-    public function selectSingle()
-    {
-        try {
-            // select all query
-            $query = "
-            SELECT ".$FEFs."
-            FROM ".$EntitySql."
-            WHERE
-                IdProfile = " . $this->IdProfile."
-            ORDER BY ".$OFs."
-            ";
-            //throw new exception($query."\n");
-            // prepare query statement
-            $Stmt = mysqli_query($this->conn, $query);
-            //throw new exception("righe trovate ".$Stmt->num_rows."\n");
-            // execute query
-            return $Stmt;
-        } catch (Exception $e) {
-            LM::LogMessage("ERROR", $e);
-            return false;
-        }
-    }
-
-    /*  IdProfile, 
-    */
 
     // create testEntity
     public function save()
@@ -101,9 +74,9 @@ class Dao
             $query = "
             INSERT INTO ".$FE."
             ".$FEFs."
-            VALUES " . $VM . "
+            VALUES " . getVMSql($FEFs, $VM) . "
             ON DUPLICATE KEY UPDATE
-            ".$LU."
+            ".getLUSql($FEFs)."
             ";
             // throw new exception($query."\n");
             // prepare query
@@ -144,8 +117,9 @@ class Dao
             // //throw new exception("righe trovate ".$Stmt->num_rows."\n");
 
             // query to delete record usr
-            $query = "DELETE from ".$FE." fe 
-                WHERE fe.".$FE." IN '" . $this->IdProfile . "'";
+            $query = "
+            DELETE from ".$this->FE." fe 
+                WHERE ".getIdFilterSql($FE, $FilterIds,$FilterType);
             //throw new exception($query."\n");
             // prepare query statement
             $Stmt = mysqli_query($this->conn, $query);
@@ -194,12 +168,11 @@ class Dao
     }
 
     /**
+     *     public $EFs; //  Entity Fields
      *     public $DEs; // Descriptive Entities
      *     public $EntitySql; // Entity
-     *     public $FilterSql; // Filter
-     *     public $EFs; //  Entity Fields
-     *     public $OFs; // Ordering  Fields
      *     public $VM; // Values Matrix 
+     *     public $FilterSql; // Filter
      *     public $LU; // List for Update 
 
      */
@@ -213,8 +186,8 @@ class Dao
                 $DEs=
                 array_map( 
                   function( $v ) { 
-                    return substr($v, 0, -1); 
-                  }, 
+                    return substr($v, 2, -1); // conventionally remove the first 2 characters 'Id'
+                }, 
                   array_filter( 
                     explode($_SESSION["DefaultSep"], $FEFs), 
                     function( $v ) { 
@@ -235,19 +208,24 @@ class Dao
     {
         try {
             // query to insert record
-            $EntitySql=$FE.' fe';
-            // $mapped = array_map('func', $values, array_keys($values));
+            $FEAlias='fe';
+            $DEAliasPrefix='de';
+            $EntitySql=$FE.' '.$FEAlias;
             if(!EN($DEs=getDEs($FEFs))){
-              $DesArr=explode(',',$DEs);
-              $EntitySql .= implode(' ',
-                array_map( 
-                  function($v, $k) { 
-                    return ' LEFT OUTER JOIN '.$v.' de'.$k.' ON de'.$k.'.'.$v.'='.'fe'.'.'.$v; 
-                  }, 
-                  $DesArr,
-                  array_keys($DesArr)
-                )
-              );
+                $DesArr=explode(',',$DEs);
+                $EntitySql .= implode(' ',
+                        array_map( 
+                            function($v, $k) { 
+                                // conventionally Entity Nam is associated to an Alias 
+                                //  fe: fundamental entity
+                                //  de<k>: descriptive entity <k> (k: 0-n integer)
+                                $DEAliases[$k]: array($v,$DEAliasPrefix.$k);
+                                return ' LEFT OUTER JOIN '.$v.' '.$DEAliases[$k].' ON '.$DEAliases[$k].'.'.$v.'='.$FEAlias.'.'.$v; 
+                            }, 
+                            $DesArr,
+                            array_keys($DesArr)
+                        )
+                );
             }
             return $EntitySql;
         } catch (Exception $e) {
@@ -260,14 +238,24 @@ class Dao
     /**
      * Type: none on null (NoN > '='), all on null (AoN > 'like')
      */
-    public function getFilterSql(string $Entity, string $Type='NoN')
+    public function getIdFilterSql(string $FE, string $FilterIds,string $Type='NoN')
     {
         try {
             // query to insert record
-            // $mapped = array_map('func', $values, array_keys($values));
-            if(!EN($Entity)){
-                $FilterSql='Id'.$Entity;
+            // IdProfile = " . $this->IdProfile."
 
+            if(!EN($FE)){
+                switch($Type) {
+                    case 'NoN': // None on Null > '=' (Single or Multiple)
+                        $FilterSql = 'fe.Id'.$FE.' IN ('.$FilterIds.')';
+                        break;
+                    case 'AoN': // All on Null > 'LIKE %' (Single)
+                        $FilterSql = 'fe.Id'.$FE." LIKE '".$FilterIds."%'";
+                        break;
+                        default:
+                        LM::LogMessage("WARNING", "FilterSql Type (".$Type.") is not correct!");
+                        break;
+                }
                 
             }
             return $FilterSql;
@@ -277,5 +265,126 @@ class Dao
         }
     }
 
+    // - FilterType: 
+    //  NoN: None on Null       > '=' (Single or Multiple Values);  Precise match
+    //  AoN: All on Null        > 'LIKE %' (Single Value);           Partial match
+    //  Min: Minimum            > >= (Single Value);
+    //  Max: Maximum            > <= (Single Value);
+    //  Rng: Range              > BETWEEN min an max (Multiple Values);
+    // Type modificator     
+    //  X: exclude              > NOT
+    //  C(dt): Cast as Datatype > CAST($var AS $dt)
+    //******************
+    // examples
+    // NoNX; AoNX;0
+
+    // FilterMatrix: array 2d : FieldNam: filterType, filterValues
+    // - $FilterMatrix = array(
+    //     "IdAlg"   => array("filterRel" => ''    , "filterType" => 'NoN', "filterValues" => '1,6,16'),
+    //     "Nam"     => array("filterRel" => 'AND' , "filterType" => 'AoN', "filterValues" => 'alg'),
+    //     "Dt"      => array("filterRel" => 'OR'  , "filterType" => 'NoN', "filterValues" => '2021-11-20'),
+    //   );
+
+    public function getFilterSql(array $FilterMatrix)
+    {
+        try {
+            // ex.
+            // IdProfile = " . $this->IdProfile."
+            // IdProfile like '" . $this->IdProfile."%'
+            // where idprj IN (1,3);
+            $FilterSql='';
+            $FilterMatrixKeys=array_keys($FilterMatrix);
+            echo "Fields:",implode(',',$FilterMatrixKeys),"<br>";
+
+            foreach ($FilterMatrixKeys as $FilterVar) {
+                $FilterRel=$FilterMatrix[$FilterVar]["filterRel"];
+                $FilterType=$FilterMatrix[$FilterVar]["filterType"];
+                $FilterVarValues=$FilterMatrix[$FilterVar]["filterValues"];
+                // echo "FilterVar       :",$FilterVar,"<br>";
+                // echo "FilterRel       :",$FilterRel,"<br>";
+                // echo "FilterType      :",$FilterType,"<br>";
+                // echo "FilterVarValues :",$FilterVarValues,"<br>";
+
+                $FilterSql .=' '.$FilterRel.' ';
+
+                if(!EN($FilterVar)){
+                    // $FilterSql='Id'.$FE;
+                    switch($FilterType) {
+                        case 'NoN': // None on Null > '=' (Single or Multiple)
+                            // $FilterSql .= ' = '.$IdFE;
+                            $FilterSql .= $FilterVar.' IN ('.$FilterVarValues.')';
+                            break;
+                        case 'AoN': // All on Null > 'LIKE %' (Single)
+                            $FilterSql .= $FilterVar." LIKE '".$FilterVarValues."%'";
+                            break;
+                        default:
+                            LM::LogMessage("WARNING", "FilterSql Type (".$Type.") is not correct!");
+                            break;
+                    }
+                }
+            }
+            
+            return $FilterSql;
+        } catch (Exception $e) {
+            LM::LogMessage("ERROR", $e);
+            return false;
+        }
+    }
+
+    // get List for Update sql 
+    public function getLUSql(string $FEFs)
+    {
+        try {
+            // query to insert record
+            // idT1=VALUES(idT1), nam=VALUES(nam), descr=VALUES(descr)
+            $LUSql = '';
+            if(!EN($FEFs)){
+                $LUSql = implode(',',
+                    array_map( 
+                        function($v) { 
+                            return $v.' = VALUES('.$v.')'; 
+                        }, 
+                        explode(',',str_replace($_SESSION["FKPostfix"],'',$FEFs))
+                    )
+                );
+            }
+            return $LUSql;
+        } catch (Exception $e) {
+            LM::LogMessage("ERROR", $e);
+            return false;
+        }
+    }
+    
+    // get List for Update sql 
+    public function getVMSql(string $FEFs, array $VM)
+    {
+        try {
+            // query to insert record
+            $FEFsArr=explode(',',$FEFs);
+            $VMSql = '';
+            if(!EN($VM)){
+                // echo 'count($VM): ', count($VM),'<br>';
+                if(count($VM)>0){
+                    // echo 'count($VM[0]): ', count($VM[0]),'<br>';
+                    // echo 'count($FEFsArr): ', count($FEFsArr),'<br>';
+                    if(count($FEFsArr)==count($VM[0])){
+                    foreach ($VM as $VMRow) {
+                        // echo 'VMRow: ', implode(',',$VMRow),'<br>';
+                        if(!EN($FEFs)){
+                            $VMSql .= '('. implode(',',$VMRow ) .'),';
+                        }
+                    }
+                    }
+                }
+            }
+
+            return substr($VMSql,0, -1);
+        } catch (Exception $e) {
+            LM::LogMessage("ERROR", $e);
+            return false;
+        }
+    }
+    
+    
 
 }
